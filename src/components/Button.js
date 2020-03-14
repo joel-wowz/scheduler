d769e52a4c6b9639b10fca1124853bcfcda82520
joelwowz <josephlafleur@hotmail.com>
2020-03-13 22:27:25 -0700

    finished project setup/familarity

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React from 'react';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
-export default function Button(props) {
-   return <></>;
-}
+export default function Button(props) {}
